f4f7f1e74813ae5eda54ce406abe139d28499a97
wangc <wangcan4096505@qq.com>
2018-04-22 22:30:03 +0800

    init

--- src/main/java/DiscardServerHandler.java
+++ src/main/java/DiscardServerHandler.java
@@ -1,27 +1,51 @@
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
 import io.netty.util.ReferenceCountUtil;
 
+/**
+ * 服务端处理通道.这里只是打印一下请求的内容，并不对请求进行任何的响应 DiscardServerHandler 继承自
+ * ChannelHandlerAdapter， 这个类实现了ChannelHandler接口， ChannelHandler提供了许多事件处理的接口方法，
+ * 然后你可以覆盖这些方法。 现在仅仅只需要继承ChannelHandlerAdapter类而不是你自己去实现接口方法。
+ * */
 public class DiscardServerHandler  extends ChannelInboundHandlerAdapter {
 
+    /**
+     * 每当收到客户端请求时 打印出请求内容
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ByteBuf in = (ByteBuf) msg;
         try {
-            while (in.isReadable()) { // (1)
-                System.out.print((char) in.readByte());
-                System.out.flush();
-            }
+            ByteBuf in = (ByteBuf) msg;
+            System.out.print(in.toString(CharsetUtil.UTF_8));
         } finally {
+            /**
+             * ByteBuf是一个引用计数对象，这个对象必须显示地调用release()方法来释放。
+             * 请记住处理器的职责是释放所有传递到处理器的引用计数对象。
+             */
+            // 抛弃收到的数据
             ReferenceCountUtil.release(msg); // (2)
         }
-        // 或者直接打印
-        System.out.println("Yes, A new client in = " + ctx.name());
     }
 
+    /**
+     * 这个方法只会在异常时触发
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        /**
+         * exceptionCaught() 事件处理方法是当出现 Throwable 对象才会被调用，即当 Netty 由于 IO
+         * 错误或者处理器在处理事件时抛出的异常时。在大部分情况下，捕获的异常应该被记录下来 并且把关联的 channel
+         * 给关闭掉。然而这个方法的处理方式会在遇到不同异常的情况下有不 同的实现，比如你可能想在关闭连接之前发送一个错误码的响应消息。
+         */
+        //关闭
         cause.printStackTrace();
         ctx.close();
     }
